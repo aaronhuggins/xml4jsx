69216a3921939c1a2803bee4088fc09128fa16ad
Aaron Huggins <aaron.huggins@gmail.com>
2022-07-02 17:23:04 -0500

    Add example and test for Nano JSX

---
+++ examples/nano_jsx.tsx
@@ -0,0 +1,22 @@
+/* @jsx x */
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import { h } from "https://deno.land/x/nano_jsx@v0.0.32/mod.ts"
+import { createXMLHandler, xml } from "../mod.ts"
+
+const x = createXMLHandler(h)
+
+export function NanoJSX () {
+  return (
+    <doc>
+      <title>Nano JSX!</title>
+      <link>https://nanojsx.io/</link>
+      <namespaceComponent></namespaceComponent>
+    </doc>
+  )
+}
+
+xml(NanoJSX, { namespaceComponent: "namespace:component" })
---
+++ examples/nano_jsx_test.tsx
@@ -0,0 +1,17 @@
+/* @jsx h */
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.32/mod.ts"
+import { createXMLRenderer } from "../mod.ts"
+import { NanoJSX } from "./nano_jsx.tsx";
+import { assertEquals } from "https://deno.land/std@0.146.0/testing/asserts.ts"
+
+Deno.test("Nano JSX XML", () => {
+  const expected = '<doc><title>Nano JSX!</title><link>https://nanojsx.io/</link><namespace:component></namespace:component></doc>'
+  const renderXML = createXMLRenderer(renderSSR)
+
+  assertEquals(renderXML(<NanoJSX />), expected)
+})
